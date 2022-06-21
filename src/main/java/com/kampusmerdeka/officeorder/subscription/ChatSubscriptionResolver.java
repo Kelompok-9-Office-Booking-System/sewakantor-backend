a084922d128eaad699a75ede7f7b312875a1721b
rofik <rofiksupriant@gmail.com>
2022-06-21 07:35:57 +0700

    dependency

---
+++ src/main/java/com/kampusmerdeka/officeorder/subscription/ChatSubscriptionResolver.java
@@ -0,0 +1,8 @@
+package com.kampusmerdeka.officeorder.subscription;
+
+import graphql.kickstart.tools.GraphQLSubscriptionResolver;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ChatSubscriptionResolver implements GraphQLSubscriptionResolver {
+}
